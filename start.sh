8e0acf3d004d6aaeef8cae5141c7825d77b074ec
Ayazhan Zhakhan <az@dropbase.io>
2024-03-29 17:14:13 -0700

    configured toml

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ docker image rm dropbase/worker
 docker build -t dropbase/worker worker/.
 
 # Set the HOST_WORKSPACE_PATH to the current directory to be used by dropbase-server
-export HOST_WORKSPACE_PATH=$(pwd)
+# export HOST_WORKSPACE_PATH=$(pwd)
 
 # Start containers
 docker-compose up
